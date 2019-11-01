1fe47f15e966ec9f4ff56dc3e6950c5e0b74868c
JeanFrancois Dube <JeanFrancois.Dube@epicgames.com>
2019-11-01 09:20:11 -0400

    FWorldBrowserModule: keep a sharedptr to the world model instead of a weakptr. #jira UE-80992 #rb francis.hurteau #fyi lauren.ridge
    [CL 9954973 by JeanFrancois Dube in 4.24 branch]

--- Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
+++ Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
@@ -3872,6 +3872,7 @@ void FEngineLoop::Exit()
 	FModuleManager::Get().UnloadModule("AssetTools", true);
 
 #endif // WITH_EDITOR
+	FModuleManager::Get().UnloadModule("WorldBrowser", true);
 	FModuleManager::Get().UnloadModule("AssetRegistry", true);
 
 #if !PLATFORM_ANDROID || PLATFORM_LUMIN 	// AppPreExit doesn't work on Android
