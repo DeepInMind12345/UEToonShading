1f982cb8ec23e7a6d639109390520412da49bc44
Matt Collins <Matt.Collins@epicgames.com>
2019-11-04 18:32:15 -0500

    Fixes GPU restart when we perform reflection captures via SceneCapture components.
    CaptureSceneToScratchCubemap performs a copy that requires depth as it has a distance fade factor. So we must use a real depth buffer for that pass.
    This is the fix for FORT-218633 plucked for 4.24.
    
    #jira FORT-218633
    #rb none
    #robomerge deadend
    
    [CL 9975739 by Matt Collins in 4.24 branch]

--- Engine/Source/Runtime/Renderer/Private/MobileShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/MobileShadingRenderer.cpp
@@ -392,7 +392,7 @@ void FMobileSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 	// See CVarMobileForceDepthResolve use in ConditionalResolveSceneDepth.
 	const bool bForceDepthResolve = CVarMobileForceDepthResolve.GetValueOnRenderThread() == 1;
 	const bool bSeparateTranslucencyActive = IsMobileSeparateTranslucencyActive(View);
-	bool bKeepDepthContent = bForceDepthResolve || (bRenderToSceneColor && (bSeparateTranslucencyActive ||
+	bool bKeepDepthContent = bForceDepthResolve || (bRenderToSceneColor && (bSeparateTranslucencyActive || View.bIsReflectionCapture ||
 		(View.bIsSceneCapture && (ViewFamily.SceneCaptureSource == ESceneCaptureSource::SCS_SceneColorHDR || ViewFamily.SceneCaptureSource == ESceneCaptureSource::SCS_SceneColorSceneDepth))));
 
 	// Whether to submit cmdbuffer with offscreen rendering before doing post-processing
