f88a021a48a576bea9a74053416c9998e4b39c20
Thomas Sarkanen <Thomas.Sarkanen@epicgames.com>
2019-11-08 06:31:13 -0500

    Fixed crash when force-deleting an in-use notify state blueprint
    #jira UE-83264 - Crash when opening the tab of an Animation who's Anim Notify State has been Force Deleted
    #rb none
    
    [CL 10080399 by Thomas Sarkanen in 4.24 branch]

--- Engine/Source/Runtime/Engine/Private/Animation/AnimInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Animation/AnimInstance.cpp
@@ -1307,7 +1307,7 @@ void UAnimInstance::TriggerAnimNotifies(float DeltaSeconds)
 	// Send end notification to AnimNotifyState not active anymore.
 	for(const FAnimNotifyEvent& AnimNotifyEvent : ActiveAnimNotifyState)
 	{
-		if (ShouldTriggerAnimNotifyState(AnimNotifyEvent.NotifyStateClass))
+		if (AnimNotifyEvent.NotifyStateClass && ShouldTriggerAnimNotifyState(AnimNotifyEvent.NotifyStateClass))
 		{
 			AnimNotifyEvent.NotifyStateClass->NotifyEnd(SkelMeshComp, Cast<UAnimSequenceBase>(AnimNotifyEvent.NotifyStateClass->GetOuter()));
 		}
