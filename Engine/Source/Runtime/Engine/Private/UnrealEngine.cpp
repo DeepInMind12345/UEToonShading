9a98a00758cf4d5dc5883000c966e19de8cc0d92
mihnea balta <Mihnea.Balta@epicgames.com>
2019-11-21 13:20:48 -0500

    Fixed: Enabling Fixed Frame Rate doesn't Lock the Frame Rate
    This was a regression introduced in CL 9842913 while trying to fix negative time deltas when switching from a fixed time step to variable (UE-81145). However, using bTimeWasManipulated to detect the switch is incorrect, because that's always true when fixed frame rate is enabled. The code now stores the previous state of bUseFixedTimeStep explicitly and compares against that to determine when the interval needs to be reset.
    
    #jira UE-84752
    #lockdown cristina.riveron
    #rb Matt.Hoffman
    #rnx
    
    [CL 10341305 by mihnea balta in 4.24 branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -1947,6 +1947,7 @@ void UEngine::UpdateTimeAndHandleMaxTickRate()
 
 															// Figure out whether we want to use real or fixed time step.
 	const bool bUseFixedTimeStep = FApp::IsBenchmarking() || FApp::UseFixedTimeStep();
+	static bool bPreviousUseFixedTimeStep = bUseFixedTimeStep;
 
 	// Updates logical last time to match logical current time from last tick
 	FApp::UpdateLastTime();
@@ -1969,11 +1970,11 @@ void UEngine::UpdateTimeAndHandleMaxTickRate()
 
 		// Did we just switch from a fixed time step to real-time?  If so, then we'll update our
 		// cached 'last time' so our current interval isn't huge (or negative!)
-		if( bTimeWasManipulated )
+		if (bUseFixedTimeStep != bPreviousUseFixedTimeStep)
 		{
-			if ( bUseFixedFrameRate )
+			if (bUseFixedFrameRate)
 			{
-				LastRealTime = CurrentRealTime - (1.0/FixedFrameRate);
+				LastRealTime = CurrentRealTime - (1.0 / FixedFrameRate);
 			}
 			else
 			{
@@ -2125,6 +2126,8 @@ void UEngine::UpdateTimeAndHandleMaxTickRate()
 			}
 		}
 	}
+
+	bPreviousUseFixedTimeStep = bUseFixedTimeStep;
 
 #if !UE_BUILD_SHIPPING
 	{
